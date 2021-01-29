7386fbdefbb4f6a972d58d6c8054c9d85b755f7a
shivamkumard107 <shivam.kumard107@gmail.com>
2021-01-30 01:45:13 +0530

    bottom navigation added

--- app/src/main/java/com/myntra/android/speciesdetectionandroid/MainActivity.kt
+++ app/src/main/java/com/myntra/android/speciesdetectionandroid/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.myntra.android.speciesdetectionandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+
     }
 }
